e15677aba9938c8b51c6b3bac25f087029917857
abhishek <abhishek4@gmail.com>
2013-12-02 21:10:13 +0100

    utils added for scale space representation

--- src/ssUtils.py
+++ src/ssUtils.py
@@ -35,87 +35,6 @@ def plotImg(data):
 	#pl.tight_layout() 
 	#pl.show()
 
-#function from David Cournapeau original scikits.learn em code
-# To plot a confidence ellipse from multi-variate gaussian pdf
-def gauss_ell(mu, va, dim = [0, 1], npoints = 100, level = 0.39):
-    """ Given a mean and covariance for multi-variate
-    gaussian, returns npoints points for the ellipse
-    of confidence given by level (all points will be inside
-    the ellipsoides with a probability equal to level)
-    
-    Returns the coordinate x and y of the ellipse"""
-    
-    c       = np.array(dim)
-
-    if mu.size < 2:
-        raise RuntimeError("this function only make sense for dimension 2 and more")
-
-    if mu.size == va.size:
-        mode    = 'diag'
-    else:
-        if va.ndim == 2:
-            if va.shape[0] == va.shape[1]:
-                mode    = 'full'
-            else:
-                raise DenError("variance not square")
-        else:
-            raise DenError("mean and variance are not dim conformant")
-
-    # If X ~ N(mu, va), then [X` * va^(-1/2) * X] ~ Chi2
-    chi22d  = stats.chi2(2)
-    mahal   = np.sqrt(chi22d.ppf(level))
-    
-    # Generates a circle of npoints
-    theta   = np.linspace(0, 2 * np.pi, npoints)
-    circle  = mahal * np.array([np.cos(theta), np.sin(theta)])
-
-    # Get the dimension which we are interested in:
-    mu  = mu[dim]
-    if mode == 'diag':
-        va      = va[dim]
-        elps    = np.outer(mu, np.ones(npoints))
-        elps    += np.dot(np.diag(np.sqrt(va)), circle)
-    elif mode == 'full':
-        va  = va[c,:][:,c]
-        #print "va = ", v a
-        # Method: compute the cholesky decomp of each cov matrix, that is
-        # compute cova such as va = cova * cova' 
-        # WARN: scipy is different than matlab here, as scipy computes a lower
-        # triangular cholesky decomp: 
-        #   - va = cova * cova' (scipy)
-        #   - va = cova' * cova (matlab)
-        # So take care when comparing results with matlab !
-        cova    = np.linalg.cholesky(va)
-        elps    = np.outer(mu, np.ones(npoints))
-        elps    += np.dot(cova, circle)
-    else:
-        raise DenParam("var mode not recognized")
-
-    return elps[0, :], elps[1, :]
-
-
-#from sklearn gmm
-def make_ellipses(bvn, ax, level=0.95):
-    for n, color in enumerate('rgb'):
-        v, w = np.linalg.eigh(bvn.covars[n][:2, :2])
-        print v, w
-        u = w[0] / np.linalg.norm(w[0])
-        angle = np.arctan(u[1]/u[0])
-        angle = 180 * angle / np.pi # convert to degrees
-        #v *= 39#25#9
-        v = 2 * np.sqrt(v * stats.chi2.ppf(level, 2)) #JP
-        ell = mpl.patches.Ellipse(bvn.mean[n, :2], v[0], v[1], 180 + angle,
-                                  facecolor='none', 
-                                  edgecolor=None, #color,
-                                  ls='dashed',
-                                  lw=3)
-        ell.set_clip_box(ax.bbox)
-        ell.set_alpha(0.5)
-        ax.add_artist(ell)
-        
-class BVN(object):
-    pass
-
 def scaleSpaceRepresentation(image, scales, octaves):
 	"""
 	Returns a Octvave-Scale matrix
@@ -148,23 +67,6 @@ def scaleSpaceRepresentation(image, scales, octaves):
 			ksize = (ks,ks)
 			Oct[i, j+1] = smoothImg(Oct[i,j], sigmaX = gau_sigma, sigmaY = gau_sigma, ksize  = ksize)
 			
-			#tempimg = Oct[i, j+1]
-
-	#print Oct[2,0]
-	# plotImg(Oct[0,0][:,:,0])
-	# plotImg(Oct[0,1])
-	# plotImg(Oct[0,2])
-	# plotImg(Oct[0,3])
-	# plotImg(Oct[1,0])
-	# plotImg(Oct[1,1])
-	# plotImg(Oct[1,2])
-	# plotImg(Oct[1,3])
-	# plotImg(Oct[2,0])
-	# plotImg(Oct[2,1])
-	# plotImg(Oct[2,2])
-	# plotImg(Oct[2,3])
-
-	# print Oct[1:,:-1].shape
 	return Oct#[1:,:-1]
 
 
@@ -382,15 +284,7 @@ def SSCS_Dist_Color(OSMatrix, sizeIn, sizeOut):
 			sig_c_col[i,j] = temparr1
 			sig_s_col[i,j] = temparr2
 
-	#print sig_c_col.shape
-	#print sig_c_col[0,0].shape
-	#print sig_c_col[0,0][0,0]
-
 	return mu_c_col, sig_c_col, mu_s_col, sig_s_col
-
-
-
-
 
 
 def normalize(arr):
@@ -421,181 +315,6 @@ def plot2DND(mean, variance):
 	plt.plot(rvs1[:, 0], rvs1[:, 1], '.')
 	plt.axis('equal')
 	plt.show()
-
-
-def csNDIntensity(image):
-	print "convert colorspace..."
-	l1 = convertColorspace(image) 
-
-	#print l1
-	print "create DOG pyramid..."
-	L,c1,c2 = getDOGPyramid(l1, level=5, sigmaX=10.5,sigmaY=10.5,ksize=(31,31))
-
-	print L
-
-	print "create supplimenting layers for intensity..."
-	iSup = []
-	for i in range(len(L)):
-		intensitySup = supplementing_layers_intensity(L[i])
-		iSup.append(intensitySup)
-
-
-	# print (iSup)
-
-	# plotImg(iSup[1][1])
-	icSup = copy.deepcopy(iSup)
-	isSup = copy.deepcopy(iSup)
-
-	print "get center gaussian for all supplimenting layers of intensity..."
-	for i in range(len(iSup)):
-		for j in range(len(iSup[i])):
-			icSup[i][j] = csEstimate(iSup[i][j], 1.0)
-
-	#print icSup[0][0]#, iSup[0][0][0,0], isSup[0][0][0,0]
-
-	#print iSup[0][0].shape
-	print "center normal distribution for all intensity layers..."	
-	# plotImg(iSup[1][1])
-	cND_Int_mu = []
-	for i in range(len(icSup)):
-		cND_Int_mu.append(icSup[i][0])
-
-	cND_Int_sigma = []
-	for i in range(len(icSup)):
-		cND_Int_sigma.append(icSup[i][1] - np.multiply(icSup[i][0], icSup[i][0]))
-
-	print cND_Int_mu[0].shape
-
-	print "get surround gaussian for all supplimenting layers of intensity..."
-	for i in range(len(iSup)):
-		for j in range(len(iSup[i])):
-			isSup[i][j] = csEstimate(iSup[i][j], 10.0)
-
-	#print icSup[0][0][0,0], iSup[0][0][0,0], isSup[0][0][0,0]
-
-	#print iSup[0][0].shape
-	print "surround normal distribution for all intensity layers..."	
-	# plotImg(iSup[1][1])
-	sND_Int_mu = []
-	for i in range(len(isSup)):
-		sND_Int_mu.append(isSup[i][0])
-
-	sND_Int_sigma = []
-	for i in range(len(isSup)):
-		sND_Int_sigma.append(isSup[i][1] - np.multiply(isSup[i][0], isSup[i][0]))
-
-
-	return cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_Int_sigma
-
-
-def csNDColor(image):
-	print "convert colorspace..."
-	l1 = convertColorspace(image) 
-
-	print "create DOG pyramid..."
-	L,c1,c2 = getDOGPyramid(l1, level=5, sigmaX=10.5,sigmaY=10.5,ksize=(31,31))
-
-
-	print "create supplimenting layers for intensity..."
-	iSup = []
-	for i in range(len(L)):
-		intensitySup = (supplementing_layers_color(c1[i], c2[i]))
-		iSup.append(intensitySup)
-
-
-	print len(iSup)
-
-	#plotImg(iSup[1][1])
-	icSup = copy.deepcopy(iSup)
-	isSup = copy.deepcopy(iSup)
-
-	print "get center gaussian for all supplimenting layers of color..."
-	for i in range(len(iSup)):
-		for j in range(len(iSup[i])):
-			icSup[i][j] = (csEstimate(iSup[i][j], 1.0))
-			#print iSup[i][j]
-
-	print icSup[0][0][0,0], iSup[0][0][0,0], isSup[0][0][0,0]
-
-	#print iSup[0][1].shape
-	print "center normal distribution for all color layers..."	
-	#plotImg(iSup[4][4])
-	cND_Int_mu = []
-
-	for i in range(len(icSup)):
-		#print i
-		line = np.empty(icSup[i][0].shape, dtype = 'object')
-		for j in range(line.shape[0]):
-			for k in range(line.shape[1]):
-				#print (j,k)
-				temparr = np.zeros((2,1))#, dtype = 'object')
-				temparr[0,0] = icSup[i][0][j,k]
-				temparr[1,0] = icSup[i][1][j,k]
-				line[j,k] = temparr#.astype(np.double)
-
-		cND_Int_mu.append(line)
-
-
-	#print cND_Int_mu[0].shape
-
-	cND_Int_sigma = []
-	for i in range(len(icSup)):
-		#print i
-		line = np.empty(icSup[i][0].shape, dtype = 'object')
-		for j in range(line.shape[0]):
-			for k in range(line.shape[1]):
-				#print (j,k)
-				temparr = np.zeros((2,2))
-				temparr[0,0] = icSup[i][2][j,k] - (icSup[i][0][j,k] ** 2)
-				temparr[0,1] = icSup[i][4][j,k] - (icSup[i][0][j,k] * icSup[i][1][j,k])
-				temparr[1,0] = icSup[i][4][j,k] - (icSup[i][0][j,k] * icSup[i][1][j,k])
-				temparr[1,1] = icSup[i][3][j,k] - (icSup[i][1][j,k] ** 2)
-				line[j,k] = temparr#.astype(np.double)
-		cND_Int_sigma.append(line)
-
-
-	print (cND_Int_sigma[0].shape)
-
-	print "get center gaussian for all supplimenting layers of color..."
-	for i in range(len(iSup)):
-		for j in range(len(iSup[i])):
-			isSup[i][j] = (csEstimate(iSup[i][j], 10.0))
-
-	print "center normal distribution for all color layers..."	
-	#plotImg(iSup[0][4])
-	sND_Int_mu = []
-	for i in range(len(isSup)):
-		line = np.empty(isSup[i][0].shape, dtype = 'object')
-		for j in range(line.shape[0]):
-			for k in range(line.shape[1]):
-				temparr = np.zeros((2,1))#, dtype = 'object')
-				temparr[0,0] = isSup[i][0][j,k]
-				temparr[1,0] = isSup[i][1][j,k]
-				line[j,k] = temparr#.astype(np.double)
-
-		sND_Int_mu.append(line)
-
-
-	#print cND_Int_mu[1].shape
-
-	sND_Int_sigma = []
-	for i in range(len(isSup)):
-		line = np.empty(isSup[i][0].shape, dtype = 'object')
-		for j in range(line.shape[0]):
-			for k in range(line.shape[1]):
-				temparr = np.zeros((2,2))#, dtype = 'object')
-				temparr[0,0] = isSup[i][2][j,k] - (isSup[i][0][j,k] ** 2)
-				temparr[0,1] = isSup[i][4][j,k] - (isSup[i][0][j,k] * isSup[i][1][j,k])
-				temparr[1,0] = isSup[i][4][j,k] - (isSup[i][0][j,k] * isSup[i][1][j,k])
-				temparr[1,1] = isSup[i][3][j,k] - (isSup[i][1][j,k] ** 2)
-				line[j,k] = temparr#.astype(np.double)
-
-		#line = line.astype(np.double)
-		sND_Int_sigma.append(line)
-
-
-
-	return cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_Int_sigma
 
 
 def SScomputeCSWassersteinIntensity(cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_Int_sigma):
@@ -631,51 +350,6 @@ def SScomputeCSWassersteinIntensity(cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_I
 			WInt[i,j] = tempimg
 
 	return WInt
-
-
-def computeCSWassersteinIntensity(cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_Int_sigma):
-	WInt = []
-
-	for i in range(len(cND_Int_mu)):
-		print i
-		tempimg = np.zeros((cND_Int_mu[i].shape))
-		for j in range(cND_Int_mu[i].shape[0]):
-			for k in range(cND_Int_mu[i].shape[1]):
-				t1 = np.linalg.norm(cND_Int_mu[i][j,k]-sND_Int_mu[i][j,k])
-				t1 = t1 * t1
-				t2 = sND_Int_sigma[i][j,k] + cND_Int_sigma[i][j,k] 
-				t3 = 2.0 * np.sqrt(np.sqrt(cND_Int_sigma[i][j,k]) * sND_Int_sigma[i][j,k] * np.sqrt(cND_Int_sigma[i][j,k]))
-
-				if (t1 + t2 - t3) < 0:
-					tempimg[j,k] = 0.0
-				else:
-					tempimg[j,k] = np.sqrt(t1 + t2 - t3)
-
-		WInt.append(tempimg)
-
-
-	return WInt
-
-def matmult (A, B):
-    rows_A = len(A)
-    cols_A = len(A[0])
-    rows_B = len(B)
-    cols_B = len(B[0])
-
-    if cols_A != rows_B:
-      print "Cannot multiply the two matrices. Incorrect dimensions."
-      return
-
-    # Create the result matrix
-    # Dimensions would be rows_A x cols_B
-    C = [[0 for row in range(cols_B)] for col in range(rows_A)]
-    #print C
-
-    for i in range(rows_A):
-        for j in range(cols_B):
-            for k in range(cols_A):
-                C[i][j] += A[i][k]*B[k][j]
-    return C
 
 
 def SScomputeCSWassersteinColor(cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_Int_sigma):
@@ -718,35 +392,7 @@ def SScomputeCSWassersteinColor(cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_Int_s
 	return WInt
 
 
-def computeCSWassersteinColor(cND_Int_mu, cND_Int_sigma, sND_Int_mu, sND_Int_sigma):
-	WInt = []
-	#print (cND_Int_mu[0].shape), (cND_Int_sigma[0].shape), (sND_Int_mu[0].shape), (sND_Int_sigma[0].shape)
-	for i in range(len(cND_Int_mu)):
-		print i
-		#print "shapeeeeeee", cND_Int_mu[0][0].shape
-		tempimg = np.zeros((cND_Int_mu[i].shape))
-		for j in xrange(cND_Int_mu[i].shape[0]):
-			for k in xrange(cND_Int_mu[i].shape[1]):
-				#print (i,j,k)
-				t1 = np.linalg.norm(cND_Int_mu[i][j,k]-sND_Int_mu[i][j,k])
-				#print t1
-				t1 = t1 ** 2.0
-				#print t1
-				t2 = np.trace(sND_Int_sigma[i][j,k]) + np.trace(cND_Int_sigma[i][j,k]) 
-				p1 = np.trace(np.dot(cND_Int_sigma[i][j,k], sND_Int_sigma[i][j,k]))
-				p2 =  np.sqrt(abs((np.linalg.det(np.dot(cND_Int_sigma[i][j,k], sND_Int_sigma[i][j,k])))))
-				t3 = 2.0 * np.sqrt(p1 + 2.0*p2)
-				#print t3
-				if (t1 + t2 - t3) < 0:
-					tempimg[j,k] = 0.0
-				else:
-					tempimg[j,k] = np.sqrt(t1 + t2 - t3)
-				#print tempimg[j,k]
 
-		WInt.append(tempimg)
-
-
-	return WInt
 def SScombineScales(WInt):
 
 
@@ -773,21 +419,7 @@ def SScombineScales(WInt):
 
 
 
-def combineScales(imglist):
-	s = len(imglist)
 
-	for i in range(s):
-		if i == 0:
-			pass
-		else:
-			imglist[i] = cv2.resize(imglist[i], (imglist[0].shape[1], imglist[0].shape[0]), interpolation=cv2.INTER_CUBIC)
-
-	img = 0
-	for i in range(s):
-		img += (np.sqrt(i+1) * imglist[i])
-
-	img = img/s
-	return img
 
 if __name__ == '__main__':
 	image = readConvert('/Users/abhishek/Documents/Thesis/pyCoDi/pyCoDi/results/orig/0_3_3327.jpg')
